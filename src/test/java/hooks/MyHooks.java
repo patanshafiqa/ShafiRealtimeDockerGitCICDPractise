13335c1f0289525aebb76e32d6f468f58bac73da
patanshafiqa <patanshafiqa@gmail.com>
2025-12-05 19:01:57 +0530

    master added after sonarquble reviw removed unused imports in the common utils

--- src/test/java/hooks/MyHooks.java
+++ src/test/java/hooks/MyHooks.java
@@ -1,18 +1,17 @@
 package hooks;
 
-import java.io.File;
-import java.io.IOException;
+
+
 import java.util.Properties;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+
+
 import org.openqa.selenium.WebDriver;
 
 import factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
+
 import utils.ConfigReader;
 
 public class MyHooks {
@@ -28,18 +27,6 @@ public class MyHooks {
 		
 	}
 	
-	/*public void AddScreenshot(Scenario scenario) throws IOException
-	{
-		WebDriver driver =testContextSetup.testBase.WebDriverManager();
-		if(scenario.isFailed())
-		{
-		//screenshot
-		File sourcePath= 	((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		byte[] fileContent = FileUtils.readFileToByteArray(sourcePath);
-		scenario.attach(fileContent, "image/png", "image");
-		
-		}*/
-	
 
 	
 	@After
@@ -48,7 +35,7 @@ public class MyHooks {
 		
 		
 		driver.quit();
-		//driver.close();
+		
 		
 	
 	}
